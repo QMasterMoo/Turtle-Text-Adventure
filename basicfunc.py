999f973c9895f87e8d235c11ca770357091fd29f
Alex Harding <cpsaicontact@gmail.com>
2013-09-11 21:35:50 -0400

    Added more functions and fixed grammar

--- basicfunc.py
+++ basicfunc.py
@@ -44,7 +44,7 @@ def down_is_right(i): #Function for making the "Down" direction correct
     elif i == "down":
         return "correct"
     else:
-        return "error 420"
+        return "error (420)"
 
 def pickup_is_right(i): #Function for making the "Pick Up" option correct
     stringit(i)
@@ -54,7 +54,7 @@ def pickup_is_right(i): #Function for making the "Pick Up" option correct
     elif i =="leave there":
         return "wrong"
     else:
-        return "error 420"
+        return "error (420)"
 
 def leavethere_is_right(i): #Function for making the "Leave there" option correct
     stringit(i)
@@ -64,7 +64,7 @@ def leavethere_is_right(i): #Function for making the "Leave there" option correc
     elif i == "leave there":
         return "correct"
     else:
-        return "error 420"
+        return "error (420)"
     
 def yes_or_no(i): #Function returning yes or no
     #OR statement removed due to it breaking the block
@@ -76,7 +76,7 @@ def yes_or_no(i): #Function returning yes or no
     elif i =="no": #or "n":
         return "no"
     else:
-        return "error 420"
+        return "error (420)"
     
 def menufunc(i): #Function managing menu
     #OR statement removed to it breaking the block
@@ -88,7 +88,7 @@ def menufunc(i): #Function managing menu
     elif i == "how to play": #or "htp":
         return "how to play"
     else:
-        return "Error 69"
+        return "Error (69)"
 
 def wake_up_is_right(i): #Wake up sequence function
     stringit(i)
@@ -119,3 +119,19 @@ def close_is_right(i): #Door open function
         return "correct"
     else:
         return "error (420)"
+
+def turn_on_is_right(i): #Turning on is right
+    if i == "turn on":
+        return "correct"
+    elif i == "keep off":
+        return "wrong"
+    else:
+        return  "Error (420)"
+
+def turn_off_is_right(i): #Keeping off is right
+    if i == "turn on":
+        return "wrong"
+    elif i == "keep off":
+        return "correct"
+    else:
+        return "Error 420"
